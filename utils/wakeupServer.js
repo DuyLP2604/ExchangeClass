016e4cbc223a3aff1f838f1e1830260ce04ab706
DuyLP2604 <duylpdce201148@gmail.com>
2025-11-15 16:06:26 +0700

    Change Server Status

--- utils/wakeupServer.js
+++ utils/wakeupServer.js
@@ -1,47 +1,43 @@
-import { server_url } from "./apiconfig.js";
+import { base_url, server_url } from "./apiconfig.js";
 
 export async function wakeupServer() {
   const serverStatus = document.getElementById("serverStatus");
-  serverStatus.textContent = "Checking server...";
-  serverStatus.classList.add("checking");
+  
+  function setStatus(text, status){
+    serverStatus.textContent = text;
+    serverStatus.className = "";
+    serverStatus.classList.add(status);
+    // classes.forEach(c => serverStatus.classList.add(c));
+  }
 
-  try {
-    const res = await fetch(server_url, { cache: "no-store" });
-    if (res.ok) {
-      serverStatus.textContent = "Server is ready!";
-      serverStatus.classList.remove("loading");
-      serverStatus.classList.add("ready");
-      serverStatus.classList.remove("checking");
+  setStatus("Checking server...", "checking");
+  const isServerRunning = async (url, timeout = 4000) => {
+    const controller = new AbortController();
+    const id = setTimeout(() => controller.abort(), timeout);
+    return fetch(url, {signal: controller.signal, cache: "no-store"})
+      .finally(() => clearTimeout(id)); 
+  };
+
+  try{
+    const res = await isServerRunning(server_url)
+    if(res.ok){
+      setStatus("Server is ready", "ready");
       return;
     }
     else{
-      console.log("Server fetch failed, waking up...");
-      serverStatus.textContent = "Waking up server";
-      serverStatus.classList.add("loading");
-      serverStatus.classList.remove("ready");
-      serverStatus.classList.remove("checking");
+      setStatus("Waking up server", "loading");
     }
-  } catch (error) {
-    console.error("Server fetch failed, waking up...");
-    serverStatus.textContent = "Waking up server";
-    serverStatus.classList.add("loading");
-    serverStatus.classList.remove("ready");
-    serverStatus.classList.remove("checking");
+  } catch (error){
+    setStatus("Waking server up", "loading");
   }
 
-  try {
-    await fetch("https://exchangeslot-api-testing.onrender.com", { cache: "no-store" });
-
-    const res2 = await fetch(server_url);
-    const data = await res2.text();
-    console.log(data);
-
-    serverStatus.textContent = "Server is ready!";
-    serverStatus.classList.remove("loading");
-    serverStatus.classList.add("ready");
-    serverStatus.classList.remove("checking");
-  } catch (error) {
-    console.error(error);
-    serverStatus.textContent = "Failed to wake up server!";
+  try{
+    await fetch(base_url);
+    const res = await fetch(server_url);
+    const data = await res.text();
+    if(res.ok) setStatus("Server is ready", "ready");
+    else setStatus("Failed to wake up server", "loading");
+  } catch(error){
+    setStatus("Failed to wake up server", "loading");
   }
 }
