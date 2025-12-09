acd1dcf7c78d699e09d8b375fe560b5804d0f4a7
DuyLP2604 <duylpdce201148@gmail.com>
2025-12-09 14:51:28 +0700

    feat: class list

--- website/classList/classList.js
+++ website/classList/classList.js
@@ -2,33 +2,85 @@ import { get_classList } from "../../utils/apiconfig.js";
 import { fetchWithAuth } from "../../utils/fetchWithAuth.js";
 
 const tableBody = document.getElementById("table_body");
+const pageInfo = document.getElementById("pageInfo");
+const previousBtn = document.getElementById("previousBtn");
+const nextBtn = document.getElementById("nextBtn");
 
-async function getClassList() {
-    tableBody.innerHTML = '';
-    tableBody.innerHTML = '<tr><td colspan="2" style="text-align:center;">Fetching class list...</td></tr>';
+let currentPage = 1;
+const PAGE_SIZE = 15;
 
-    try{
+async function getClassList(page) {
+    tableBody.innerHTML = `
+        <tr><td colspan="2" style="text-align:center;">Fetching class list...</td></tr>
+    `;
+
+    try {
         const res = await fetchWithAuth(get_classList(page));
-        const json = await res.json();
-        const data = json.data;
-        if(res.ok){
-            data.forEach((item) => {
-                const row = `
-                        <tr>
-                            <td>${item.classCode}</td>
-                            <td>${item.slot}</td>
-                        </tr>
-                    `;
-                tableBody.innerHTML += row;
-            });
+        const data = await res.json();
+
+        if (!res.ok || data.processSuccess === false) {
+            tableBody.innerHTML = `
+                <tr><td colspan="2" style="text-align:center;">${data.message}</td></tr>
+            `;
+            return null;
         }
-        else{
-            tableBody.innerHTML = `<tr><td colspan="2" style="text-align:center;">${json.message}</td></tr>`;
-        }
-    }catch(error){
-        console.error(error);
+
+        return data.data;
+    } catch (err) {
+        console.error(err);
         alert("Cannot connect to server");
+        return null;
     }
 }
 
-window.addEventListener("load", getClassList);
+function displayClassList(list) {
+    tableBody.innerHTML = "";
+
+    if (!list || list.length === 0) {
+        tableBody.innerHTML = `
+            <tr><td colspan="2" style="text-align:center;">No classes available.</td></tr>
+        `;
+        return;
+    }
+
+    list.forEach(item => {
+        tableBody.innerHTML += `
+            <tr>
+                <td>${item.classCode}</td>
+                <td>${item.slot}</td>
+            </tr>
+        `;
+    });
+}
+
+function updatePagination(length) {
+    pageInfo.innerHTML = `Page ${currentPage}`;
+    previousBtn.disabled = currentPage === 1;
+    nextBtn.disabled = length < PAGE_SIZE;
+}
+
+async function loadClassList(page) {
+    currentPage = page;
+    const backendPage = page - 1;
+
+    const list = await getClassList(backendPage);
+
+    if (list) {
+        displayClassList(list);
+        updatePagination(list.length);
+    } else {
+        displayClassList([]);
+        updatePagination(0);
+    }
+}
+
+previousBtn.addEventListener("click", () => {
+    if (currentPage > 1) loadClassList(currentPage - 1);
+});
+
+nextBtn.addEventListener("click", () => {
+    loadClassList(currentPage + 1);
+});
+
+
+loadClassList(1);
