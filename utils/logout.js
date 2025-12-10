f357475759892762201ada35efda2e1805128259
DuyLP2604 <duylpdce201148@gmail.com>
2025-12-10 23:09:50 +0700

    feat: progress bar for log out

--- utils/logout.js
+++ utils/logout.js
@@ -1,7 +1,10 @@
 import { logout_url } from "./apiconfig.js";
 import { fetchWithAuth } from "./fetchWithAuth.js";
+import { finishProgressBar } from "./finishProgressBar.js";
+import { startProgressBar } from "./startProgressBar.js";
 
 export async function callLogout() {
+    startProgressBar();
     try {
         const res = await fetchWithAuth(logout_url, {
             method: "POST",
@@ -11,11 +14,12 @@ export async function callLogout() {
         if (!res.ok) {
             throw new Error("Logout failed");
         }
-
+        finishProgressBar();
         return true;
     } catch (error) {
         console.error(error);
         alert("Cannot logout due to some network issue");
+        finishProgressBar();
         return false;
     }
 }
