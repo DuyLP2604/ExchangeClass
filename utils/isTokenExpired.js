167d9c139ecc62e1ab67b009e4a0050c55ee731d
DuyLP2604 <duylpdce201148@gmail.com>
2025-12-10 19:10:35 +0700

    fix: -----------

--- utils/isTokenExpired.js
+++ utils/isTokenExpired.js
@@ -1,8 +1,19 @@
-export function isTokenExpired(token){
-    if(!token) return true;
+export function isTokenExpired(token) {
+    if (!token) return true;
 
-    const payload = JSON.parse(atob(token.split('.')[1]));
-    const now = Date.now() / 1000;
+    try {
+        const base64 = token.split('.')[1]
+            .replace(/-/g, '+')
+            .replace(/_/g, '/');
 
-    return payload.exp < now;
+        const payload = JSON.parse(atob(base64));
+        const now = Date.now() / 1000;
+
+        if (!payload.exp) return true;
+
+        return payload.exp < now;
+    } catch (err) {
+        console.error("Invalid token:", err);
+        return true; 
+    }
 }
